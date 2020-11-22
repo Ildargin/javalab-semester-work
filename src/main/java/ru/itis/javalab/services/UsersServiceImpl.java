0695720b06d69ed2ae0b4412225d0cf3f0416254
Ildar Gainatullin <ildar.gainatullin@gmail.com>
2020-11-22 21:58:21 +0300

    add starter template from previous project

---
+++ src/main/java/ru/itis/javalab/services/UsersServiceImpl.java
@@ -0,0 +1,42 @@
+package ru.itis.javalab.services;
+
+import ru.itis.javalab.dto.UserDto;
+import ru.itis.javalab.models.User;
+import ru.itis.javalab.repositories.UsersRepository;
+
+import java.util.List;
+
+import static ru.itis.javalab.dto.UserDto.from;
+
+
+public class UsersServiceImpl implements UsersService {
+
+    private UsersRepository usersRepository;
+
+    public UsersServiceImpl(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public List<UserDto> getAllUsers() {
+        return UserDto.from(usersRepository.findAll());
+    }
+
+    @Override
+    public List<UserDto> getAllUsers(int page, int size) {
+        return from(usersRepository.findAll(page, size));
+    }
+
+    @Override
+    public void addUser(UserDto userDto) {
+        usersRepository.save(User.builder()
+                .firstName(userDto.getFirstName())
+                .lastName(userDto.getLastName())
+                .build());
+    }
+
+    @Override
+    public UserDto getUser(Long userId) {
+        return UserDto.from(usersRepository.findById(userId).orElse(null));
+    }
+}
