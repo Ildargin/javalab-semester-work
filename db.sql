d28870d3a68fb653c593aa567314ef21c5293554
Ildar Gainatullin <ildar.gainatullin@gmail.com>
2020-11-24 16:57:02 +0300

    update sql map;

--- db.sql
+++ db.sql
@@ -2,7 +2,7 @@ create database semester_work;
 
 create table "user"
 (
-    user_id    bigserial primary key,
+    id serial primary key,
     first_name varchar(40),
     last_name  varchar(40),
     email      varchar(40),
@@ -10,22 +10,25 @@ create table "user"
     birth_date date
 );
 
-create table "comment"
-(
-    comment_id bigserial primary key,
-    user_id  int,
-    post_id  int,
-    comment_text  varchar(200),
-    constraint user_fk foreign key(user_id) references "user"(user_id),
-    constraint post_fk foreign key(user_id) references post(post_id)
-);
 
 create table "post"
 (
-    post_id bigserial primary key,
+    id serial primary key,
     post_title  varchar(70),
     post_text  varchar(1000),
     comment_text  varchar(200),
-    image_source varchar(100)
+    image_source varchar(100),
+    likes    int,
+    dislikes int
+);
+
+create table "comment"
+(
+    comment_id serial primary key,
+    user_id  int,
+    post_id  int,
+    comment_text  varchar(200),
+    constraint user_fk foreign key(user_id) references "user"(id),
+    constraint post_fk foreign key(post_id) references post(id)
 );
 
